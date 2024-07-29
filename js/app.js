01d0f48bef35207b88b821db6e6a4c6f7824ec9d
Haroon Arshad <arshadharoon216@gmail.com>
2024-07-29 12:18:52 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const iBar = document.getElementById('icon')
+const script = document.querySelector(".scrolBar");
+
+
+iBar.addEventListener('click', () => {    
+    script.classList.toggle('scroll')
+})
+
